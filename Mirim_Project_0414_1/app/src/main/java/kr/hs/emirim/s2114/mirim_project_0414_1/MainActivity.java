adc63c66d049f52f138d6c64dd5c6285123e0e21
yscho010612@gmail.com <yscho010612@gmail.com>
2022-04-19 19:26:30 +0900

    체크박스 상태에 따라 보이게 또는 보이지 않게 설정

--- Mirim_Project_0414_1/app/src/main/java/kr/hs/emirim/s2114/mirim_project_0414_1/MainActivity.java
+++ Mirim_Project_0414_1/app/src/main/java/kr/hs/emirim/s2114/mirim_project_0414_1/MainActivity.java
@@ -3,12 +3,40 @@ package kr.hs.emirim.s2114.mirim_project_0414_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+    CheckBox checkStart;
+    ImageView imgv;
+    LinearLayout linearsub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linearsub = findViewById(R.id.linear_sub);
+        rg = findViewById(R.id.rg);
+        checkStart = findViewById(R.id.check_start);
+        imgv = findViewById(R.id.imgv);
+        Button btnDone = findViewById(R.id.but_done);
+
+        checkStart.setOnCheckedChangeListener(checkListener);
     }
+    CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+            if(isChecked){
+                linearsub.setVisibility(View.VISIBLE);
+            }else{
+                linearsub.setVisibility(View.INVISIBLE);
+            }
+        }
+    };
 }
