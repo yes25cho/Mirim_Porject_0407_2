228d1ce2f608bd8c2f39b9c2bdaccdda8deebcda
yscho010612@gmail.com <yscho010612@gmail.com>
2022-05-19 19:56:15 +0900

    Clock, TimePicker, DatePicker 연습

---
+++ Mirim_Project_05192/app/src/main/java/mediapoint/project/mirim_project_05192/MainActivity.java
@@ -0,0 +1,14 @@
+package mediapoint.project.mirim_project_05192;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_datepicker);
+    }
+}
