f7397d85ba49e83ec68b4c8afdaa56340aaf1c79
yscho010612@gmail.com <yscho010612@gmail.com>
2022-05-19 19:40:26 +0900

    add ImageView_TextView

--- Mirim_Project_0519_1/app/src/main/java/mediapoint/project/mirim_project_0519_1/MainActivity.java
+++ Mirim_Project_0519_1/app/src/main/java/mediapoint/project/mirim_project_0519_1/MainActivity.java
@@ -3,12 +3,47 @@ package mediapoint.project.mirim_project_0519_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear_red, linear_yellow, linear_blue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear_red = findViewById(R.id.linear_red);
+        linear_yellow = findViewById(R.id.linear_yellow);
+        linear_blue = findViewById(R.id.linear_blue);
+
+        Button btnRed = findViewById(R.id.btn_red);
+        Button btnYellow = findViewById(R.id.btn_yellow);
+        Button btnBlue = findViewById(R.id.btn_blue);
+
+        btnRed.setOnClickListener(btnListener);
+        btnYellow.setOnClickListener(btnListener);
+        btnBlue.setOnClickListener(btnListener);
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            linear_red.setVisibility(View.INVISIBLE);
+            linear_yellow.setVisibility(View.INVISIBLE);
+            linear_blue.setVisibility(View.INVISIBLE);
+            switch (view.getId()){
+                case R.id.btn_red:
+                    linear_red.setVisibility(view.VISIBLE);
+                    break;
+                case R.id.btn_yellow:
+                    linear_yellow.setVisibility(view.VISIBLE);
+                    break;
+                case R.id.btn_blue:
+                    linear_blue.setVisibility(view.VISIBLE);
+                    break;
+            }
+        }
+    };
+
 }
