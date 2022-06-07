d8e27a5275a123b30542fa2213f13ea3abf9d891
yscho010612@gmail.com <yscho010612@gmail.com>
2022-06-07 20:07:53 +0900

    직접 풀어보기 6-1 버튼 제거

--- Mirim_Project_0519_3/app/src/main/java/mediapoint/project/mirim_project_0519_3/MainActivity.java
+++ Mirim_Project_0519_3/app/src/main/java/mediapoint/project/mirim_project_0519_3/MainActivity.java
@@ -32,10 +32,8 @@ public class MainActivity extends AppCompatActivity {
         time = findViewById(R.id.time);
         date = findViewById(R.id.date);
         textResult = findViewById(R.id.text_result);
-        Button btnStart = findViewById(R.id.btn_start);
-        Button btnDone = findViewById(R.id.btn_done);
-        btnStart.setOnClickListener(btnListener);
-        btnDone.setOnClickListener(btnListener);
+        timer.setOnClickListener(timerListener);
+        textResult.setOnLongClickListener(textListener);
         rg.setOnCheckedChangeListener(rgListener);
         date.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
             @Override
@@ -63,7 +61,24 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-
+    View.OnClickListener timerListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            timer.setBase(SystemClock.elapsedRealtime());
+            timer.start();
+            timer.setTextColor(Color.RED);
+        }
+    };
+    View.OnLongClickListener textListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View view) {
+            timer.stop();
+            timer.setTextColor(Color.BLUE);
+            textResult.setText(selectedYear+"년 "+selectedMonth+"월 "+selectedDay+"일 ");
+            textResult.append(time.getCurrentHour()+"시 "+time.getCurrentMinute()+"분 예약 완료됨");
+            return true;
+        }
+    };
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
