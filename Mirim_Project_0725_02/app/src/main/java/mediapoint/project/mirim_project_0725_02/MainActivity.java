55a4d484c08b6c9c9456a1ce8d9b08e964c9fc0e
yscho010612@gmail.com <yscho010612@gmail.com>
2022-07-25 11:50:04 +0900

    웹뷰 초기 커밋

---
+++ Mirim_Project_0725_02/app/src/main/java/mediapoint/project/mirim_project_0725_02/MainActivity.java
@@ -0,0 +1,14 @@
+package mediapoint.project.mirim_project_0725_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);ㅁ
+        setContentView(R.layout.activity_main);
+    }
+}
