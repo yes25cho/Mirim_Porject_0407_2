9b0f435d944426f5949d612162271cc66fe71ea6
yscho010612@gmail.com <yscho010612@gmail.com>
2022-07-26 09:29:55 +0900

    메뉴 완성 commit

--- Mirim_Project_0725_3/app/src/main/java/mediapoint/project/mirim_project_0725_3/MainActivity.java
+++ Mirim_Project_0725_3/app/src/main/java/mediapoint/project/mirim_project_0725_3/MainActivity.java
@@ -2,16 +2,24 @@ package mediapoint.project.mirim_project_0725_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear1;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear1 = findViewById(R.id.linear1);
+        btn = findViewById(R.id.btn);
     }
 
     @Override
@@ -20,5 +28,26 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.optionmenu, menu);
         return true;
+    }//메뉴 객체 생성
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.item_red:
+                linear1.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_yellow:
+                linear1.setBackgroundColor(Color.YELLOW);
+                return true;
+            case R.id.item_green:
+                linear1.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.btn_rotate:
+                btn.setRotation(45);
+                return true;
+            case R.id.btn_size:
+                btn.setScaleX(2);
+                return true;
+        }
+        return false;
     }
 }
