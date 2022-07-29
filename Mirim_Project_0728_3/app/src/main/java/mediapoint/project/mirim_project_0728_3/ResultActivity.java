2a4ade4d60a684abd8ef87f5a6d8b3c88b8a7fac
yscho010612@gmail.com <yscho010612@gmail.com>
2022-07-29 10:22:16 +0900

    ResultActivity 수정 완성

--- Mirim_Project_0728_3/app/src/main/java/mediapoint/project/mirim_project_0728_3/ResultActivity.java
+++ Mirim_Project_0728_3/app/src/main/java/mediapoint/project/mirim_project_0728_3/ResultActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -16,8 +17,23 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         Intent intent = getIntent();
+        ImageView  imgv = findViewById(R.id.imgv);
+        TextView textv= findViewById(R.id.textv);
         String[] imgName = intent.getStringArrayExtra("imgName");
         int[] voteCount = intent.getIntArrayExtra("voteCount");
+        int[] imgRes = {R.drawable.img01,R.drawable.img02,R.drawable.img03,
+                R.drawable.img04,R.drawable.img05,R.drawable.img06,
+                R.drawable.img07,R.drawable.img08,R.drawable.img09};
+        int max = voteCount[0];
+        int maxId = 0;
+        for(int i =1; i<voteCount.length; i++){
+            if(max<voteCount[i]){
+                maxId = i;
+                max = voteCount[i];
+            }
+        }
+        imgv.setImageResource(imgRes[maxId]);
+        textv.setText(imgName[maxId]);
         TextView[] tv = new TextView[imgName.length];
         int[] textId = {R.id.text01, R.id.text02, R.id.text03,
                 R.id.text04, R.id.text05, R.id.text06,
