0f9146a90869e5d2c64c9ce1a57c0ca40a29d51e
yscho010612@gmail.com <yscho010612@gmail.com>
2022-08-05 13:37:56 +0900

    맵연결 성공

---
+++ Mirim_Project_0805_MapApp/app/src/main/java/mediapoint/project/mirim_project_0805_mapapp/MainActivity.java
@@ -0,0 +1,13 @@
+package mediapoint.project.mirim_project_0805_mapapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
